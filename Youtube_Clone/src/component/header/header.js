ef4b3bbd5df9c3067d8e8967b7b266f88feab6ed
Gautam Dhakate <106535248+Gautam-121@users.noreply.github.com>
2023-10-12 07:26:21 +0530

    youtube_search_wrapper_done

--- Youtube_Clone/src/component/header/header.js
+++ Youtube_Clone/src/component/header/header.js
@@ -1,22 +1,36 @@
-import "./header.css"
+import "./header.css";
 
-const Header = ()=>{
-    return(
-        <div className="header_wrapper">
-            <div className="header_logo_conatiner">
-            <i class="fa-solid fa-bars"></i>
-             <div className="logo_wrapper">
-                   <div className="logo"></div>
-               </div>
-            </div>
-            <div className="header_search_container">
-
-            </div>
-            <div className="header_signIn_container">
-
-            </div>
+const Header = () => {
+  return (
+    <div className="header_wrapper">
+      <div className="header_logo_conatiner">
+        <i class="fa-solid fa-bars menu_bar"></i>
+        <div className="logo_wrapper">
+          <div className="logo"></div>
         </div>
-    )
-}
+      </div>
+      <div className="header_search_container">
+        <div className="search_wrapper">
+          <input
+            type="text"
+            placeholder="Search"
+            value=""
+            name=""
+            class="search_bar"
+          />
+          <div className="search_Wrapper">
+            <div className="search_icon">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <div className="mike_icon">
+              <i class="fa-solid fa-microphone"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header_signIn_container"></div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
