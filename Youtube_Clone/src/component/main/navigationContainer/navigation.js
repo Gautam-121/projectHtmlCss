e0898d1e4ce244cfeff7ea2b69299d7f171c9229
Gautam Dhakate <106535248+Gautam-121@users.noreply.github.com>
2023-10-13 07:29:15 +0530

    navigation menu start

--- Youtube_Clone/src/component/main/navigationContainer/navigation.js
+++ Youtube_Clone/src/component/main/navigationContainer/navigation.js
@@ -1,22 +1,61 @@
-import "./navigation.css"
+import "./navigation.css";
 
-const NavigationContainer = ()=>{
-    return(
-        <div className="navigation_wrapper">
-            <div className="box1">
-                <div className="home">
-                   <i class="fa-solid fa-house"></i>
-                   <p>Home</p>
-                </div>
-                 <div className="home">
-                    box2
-                </div>
-                <div className="home">
-                    box3
-                </div>
-            </div>
+const NavigationContainer = () => {
+  return (
+    <div className="navigation_wrapper">
+      <div className="box1">
+        <div className="home background">
+          <i class="fa-solid fa-house"></i>
+          <p>Home</p>
         </div>
-    )
-}
+        <div className="home">
+          <i class="fa-solid fa-house"></i>
+          <p>Shorts</p>
+        </div>
+        <div className="home">
+          <i class="fa-solid fa-house"></i>
+          <p>Subscriptions</p>
+        </div>
+      </div>
+      <div className="box1">
+        <div className="home background">
+          <i class="fa-solid fa-house"></i>
+          <p>Home</p>
+        </div>
+        <div className="home">
+          <i class="fa-solid fa-house"></i>
+          <p>Shorts</p>
+        </div>
+      </div>
+      <div className="box1">
+        <div className="home background">
+          Sign in to like videos, comment, and subscribe.
+        </div>
+        <div className="header_signIn_container">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+          <div className="signIn_Wrapper">
+            <i class="fa-regular fa-user"></i>
+            sign in
+          </div>
+        </div>
+      </div>
+      <div className="box1">
+        <p className="title">Explore</p>
+        <div className="home background">
+          <i class="fa-solid fa-house"></i>
+          <p>Home</p>
+        </div>
+        <div className="home">
+          <i class="fa-solid fa-house"></i>
+          <p>Shorts</p>
+        </div>
+        <div className="home">
+          <i class="fa-solid fa-house"></i>
+          <p>Subscriptions</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavigationContainer
+export default NavigationContainer;
