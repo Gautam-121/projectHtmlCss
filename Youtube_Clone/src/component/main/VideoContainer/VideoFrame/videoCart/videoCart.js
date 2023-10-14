ae74ae560f2903903d126be7862d3bf85abb1f76
Gautam Dhakate <106535248+Gautam-121@users.noreply.github.com>
2023-10-14 19:55:46 +0530

    videoCart Done

--- Youtube_Clone/src/component/main/VideoContainer/VideoFrame/videoCart/videoCart.js
+++ Youtube_Clone/src/component/main/VideoContainer/VideoFrame/videoCart/videoCart.js
@@ -6,12 +6,19 @@ const VideoCart = ()=>{
             <div className="video_Cart">
                 <img alt="this is img" src="https://i.ytimg.com/vi/vFxQyZX84Ro/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAsighpJ967W94onqVm-q1xZ_Zo7w" className="video_img" width="250px" height="150px"/>
                 <div className="video_info_wrapper">
-                    <img src="" alt="" />
+                    <img src="https://yt3.ggpht.com/q4pQdGZdT9Suk5Yu7cm0KI9pLMSaFhEeKQtyZCYjFeJRcbODjS4V5J9AQhN96TYOHXI-rgZ5TA=s68-c-k-c0x00ffffff-no-rj" alt="ytbuber logo" width="40px" height="40px" />
                     <div className="video_name_info_wrapper">
-                        <p>How Dukkan Moved Out Of Cloud and are hosting their infra on Bare Metal ft...</p>
-                        <p>Asli Engineering by Arpit</p>
+                        <span className="title_video">How Dukkan Moved Out Of Cloud and are hosting their infra on Bare Metal ft...</span>
+                        <div className="info_wrapper">
+                        <span>Asli Engineering by Arpit Bhayani</span>
+                        <br />
+                        <div className="view_wrapper">
+                            <span>20K views</span>
+                            <span class="dot"></span>
+                             <span>1 day ago</span>
+                        </div>
+                        </div>
                     </div>
-
                 </div>
             </div>
             <div className="video_Cart">
---
+++ Youtube_Clone/src/utils/videoContainerInfo.js
@@ -0,0 +1,10 @@
+const videoInfo = [
+    {
+        videoTumbnail : "",
+        profileLogo : "",
+        title : "",
+        ytbName : "",
+        view : "",
+        ago : ""
+    }
+]
